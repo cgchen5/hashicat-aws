bed1d47bc156b3394b911377d2404085d16f653c
cg chen <cgchen5@yahoo.com.au>
2021-11-12 05:04:40 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,6 @@
-module "s3-bucket_example_complete" {
-  source  = "app.terraform.io/cchen-training-3/s3-bucket/aws//examples/complete"
+module "s3-bucket" {
+  source  = "app.terraform.io/cchen-training-3/s3-bucket/aws"
   version = "2.2.0"
+  # insert required variables here
+  bucket_prefix = "cchen"
 }
